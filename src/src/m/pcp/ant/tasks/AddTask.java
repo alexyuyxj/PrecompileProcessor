0a42c494a52742e5eacc79871e9422d77ba54744
Alex Yu <alexyu.yxj@gmail.com>
2015-05-28 10:18:07 +0800

    添加项目

---
+++ src/src/m/pcp/ant/tasks/AddTask.java
@@ -0,0 +1,32 @@
+package m.pcp.ant.tasks;
+
+import m.pcp.ExpressionParser;
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+
+public class AddTask extends Task {
+	private String list;
+	private String value;
+	
+	public void setList(String list) {
+		this.list = list;
+	}
+	
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	public void execute() throws BuildException {
+		String listSize = getProject().getProperty(list + "#size");
+		if (listSize == null) {
+			throw new BuildException(list + " is not defined");
+		}
+		
+		int size = Integer.parseInt(listSize);
+		getProject().setProperty(list + "#size", String.valueOf(size + 1));
+		
+		ExpressionParser parser = new ExpressionParser();
+		getProject().setProperty(list + "#" + size, parser.parse(value));
+	}
+	
+}
---
+++ src/src/m/pcp/ant/tasks/BreakTask.java
@@ -0,0 +1,12 @@
+package m.pcp.ant.tasks;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+
+public class BreakTask extends Task {
+
+	public void execute() throws BuildException {
+		LoopTask.pushBreak();
+	}
+	
+}
